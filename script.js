db947287b087384e3571354a34b8ab6f4988d6fa
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-05-04 16:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ window.promises = [];
 // Do not change the code above this
 // add your promises to the array `promises`
 
-
-const promises = Array.from({length: 5}, () => new Promise(resolve =>
+promises = Array.from({length: 5}, () => new Promise(resolve =>
   setTimeout(() => resolve(Math.floor(Math.random() * 5) + 1), Math.random() * 5000)
 ));
 
